c4f9954fee27e672e06c2441a69d221799ef9f93
BM <new2knew@hotmail.com>
2018-02-10 21:30:33 +0200

    Add BankAccount class

---
+++ PrivateBankingSystem/PrivateBankingSystem/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateBankingSystem
+{
+    class BankAccount
+    {
+        public string LoggedUser { get; set; }
+        public string TypeOfTransaction { get; set; }
+        public DateTime TansactionTime { get; set; }
+        public string AccountHolder { get; set; }
+    }
+}
